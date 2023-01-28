5f398c802fd6d01653b08c18039f722c6316a0dd
zaida cueto <zcueto58@gmail.com>
2023-01-27 23:00:02 -0300

    Criando a classe posiçaõ

---
+++ XadrezHubGames/XadrezHubGames/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using Tabuleiro;
+
+namespace XadrezHubGames
+{ 
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            Posicao p;
+            p =  new Posicao(3, 4);
+
+            Console.WriteLine("Posiçao:" + p);
+
+            Console.ReadKey();
+
+        }
+    }
+}
