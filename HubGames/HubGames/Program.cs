c3ba3aa8bb19041cceeced0dc7f93f17ba7494cc
ZaidaCueto <zcueto58@gmail.com>
2023-01-29 22:33:00 -0300

    Criando a classe PosicaoXadrez

--- HubGames/HubGames/Program.cs
+++ HubGames/HubGames/Program.cs
@@ -11,23 +11,10 @@ namespace XadrezHubGames
         public static void Main(string[] args)
         {
 
-            try
-            {
-                Tabeleiro tab = new Tabeleiro(8, 8);
-                tab.colocarPeca(new Torre(tab, Cor.PRETA), new Posicao(0, 0));
-                tab.colocarPeca(new Torre(tab, Cor.PRETA), new Posicao(1, 3));
-                tab.colocarPeca(new Rei(tab, Cor.PRETA), new Posicao(0, 0));
+          PosicaoXadrez pos = new PosicaoXadrez ( 'c', 7);
 
-                TelaXadrez.imprimirTabuleiro(tab);
-            }
-            catch (TabuleiroException e)
-            {
-                Console.WriteLine(e.Message);
-
-            }
-
-
-          
+            Console.WriteLine(pos);
+            Console.WriteLine(pos.toPosicao);
 
             Console.ReadKey();
 
