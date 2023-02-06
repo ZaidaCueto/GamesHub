ae93d00d88f395b2c183196da557061247d8ad6e
ZaidaCueto <zcueto58@gmail.com>
2023-02-05 23:53:46 -0300

    Correção: mudando o nome do metodo para movimetoPossivel

--- HubGames/HubGames/xadrez/model/PartidaDeXadrez.cs
+++ HubGames/HubGames/xadrez/model/PartidaDeXadrez.cs
@@ -12,19 +12,62 @@ namespace model
         public bool terminada { get; private set; }
         private HashSet<Peca> pecas;
         private HashSet<Peca> capturadas;
-
+        public bool  xaque { get; private set; }
         public PartidaDeXadrez()
         {
             tab = new Tabuleiro(8, 8);
             turno = 1;
-            jogadorAtual = Cor. MAGENTA;
+            jogadorAtual = Cor.MAGENTA;
             terminada = false;
             pecas = new HashSet<Peca>();
             capturadas = new HashSet<Peca>();
             ColocarPecas();
+            xaque = false;
         }
 
-        public void ExecutarMovimento(Posicao origem, Posicao destino)
+
+        private Cor adversaria(Cor cor)
+        {
+            if (cor == Cor.MAGENTA)
+            {
+                return Cor.AMARELO;
+            }
+            else
+            {
+                return Cor.MAGENTA;
+            }
+        }
+
+        private Peca rei(Cor cor)
+        {
+            foreach (Peca x in pecasEmJogo(cor))
+            {
+                if (x is Rei)
+                {
+                    return x;
+                }
+            }
+            return null;
+        }
+
+        public bool estaEmXeque(Cor cor)
+        {
+            Peca R = rei(cor);
+            if (R == null)
+            {
+                throw new TabuleiroException("Não tem rei da cor " + cor + " no tabuleiro!");
+            }
+            foreach (Peca x in pecasEmJogo(adversaria(cor)))
+            {
+                bool[,] matriz = x.movimentosPossiveis();
+                if (matriz[R.posicao.linha, R.posicao.coluna])
+                {
+                    return true;
+                }
+            }
+            return false;
+        }
+        public Peca ExecutarMovimento(Posicao origem, Posicao destino)
         {
             Peca p = tab.RetirarPeca(origem);
             p.incrementarQtdMovimentos();
@@ -34,14 +77,41 @@ namespace model
             {
                 capturadas.Add(pecaCapturada);
             }
+            return pecaCapturada;
+        }
+
+        private void desfazMovimento(Posicao origem, Posicao destino, Peca pecaCapturada)
+        {
+            Peca p = tab.RetirarPeca(destino);
+            p.deCrementarQtdMovimentos();
+            if(pecaCapturada != null)
+            {
+                tab.ColocarPeca(pecaCapturada, destino);
+                capturadas.Remove(pecaCapturada);
+            }
+            tab.ColocarPeca(p, origem);
+
         }
 
         public void realizaJogada(Posicao origem, Posicao destino)
         {
-            ExecutarMovimento(origem, destino);
+        Peca pecaCapturada = ExecutarMovimento(origem, destino);
+
+            if (estaEmXeque(jogadorAtual))
+            {
+                desfazMovimento(origem, destino, pecaCapturada);
+                throw new TabuleiroException("Você não pode se colocar em xaque");
+            }
+           if()
+            {
+                (estaEmXeque(adversaria(jogadorAtual)))
+                
+            }
             turno++;
             nudarJogador();
         }
+
+      
 
         public void validarPosicaoDeOrigem(Posicao pos)
         {
@@ -49,11 +119,11 @@ namespace model
             {
                 throw new TabuleiroException("Não existe peça na posição de origem escolhida!");
             }
-            if(jogadorAtual != tab.peca(pos).cor)
+            if (jogadorAtual != tab.peca(pos).cor)
             {
                 throw new TabuleiroException("não é sua vez!");
             }
-            if(!tab.peca(pos).existeMovimentosPossiveis())
+            if (!tab.peca(pos).existeMovimentosPossiveis())
             {
                 throw new TabuleiroException("Não há movimentos possíveis para de origem escolhida!");
             }
@@ -61,7 +131,7 @@ namespace model
 
         public void validarPosicaoDeDestino(Posicao origem, Posicao destino)
         {
-if(!tab.peca(origem).podeMoverPara(destino))
+            if (!tab.peca(origem).podeMoverPara(destino))
             {
                 throw new TabuleiroException("Posicão de destino inválido!");
                 throw new TabuleiroException("Digete outra posição!");
@@ -70,13 +140,13 @@ if(!tab.peca(origem).podeMoverPara(destino))
 
         private void nudarJogador()
         {
-      if (jogadorAtual == Cor. MAGENTA)
+            if (jogadorAtual == Cor.MAGENTA)
             {
                 jogadorAtual = Cor.AMARELO;
             }
             else
             {
-                jogadorAtual = Cor. MAGENTA;
+                jogadorAtual = Cor.MAGENTA;
             }
 
         }
@@ -104,8 +174,8 @@ if(!tab.peca(origem).podeMoverPara(destino))
                     aux.Add(x);
                 }
             }
-         aux.ExceptWith(pecasCapturadas(cor));
-            return aux;    
+            aux.ExceptWith(pecasCapturadas(cor));
+            return aux;
         }
 
 
@@ -116,12 +186,12 @@ if(!tab.peca(origem).podeMoverPara(destino))
         }
         private void ColocarPecas()
         {
-            tab.ColocarPeca(new Torre(tab, Cor. MAGENTA), new PosicaoXadrez('c', 1).toPosicao());
-            tab.ColocarPeca(new Torre(tab, Cor. MAGENTA), new PosicaoXadrez('c', 2).toPosicao());
-            tab.ColocarPeca(new Torre(tab, Cor. MAGENTA), new PosicaoXadrez('d', 2).toPosicao());
-            tab.ColocarPeca(new Torre(tab, Cor. MAGENTA), new PosicaoXadrez('e', 2).toPosicao());
-            tab.ColocarPeca(new Torre(tab, Cor. MAGENTA), new PosicaoXadrez('e', 1).toPosicao());
-            tab.ColocarPeca(new Rei(tab, Cor. MAGENTA), new PosicaoXadrez('d', 1).toPosicao());
+            tab.ColocarPeca(new Torre(tab, Cor.MAGENTA), new PosicaoXadrez('c', 1).toPosicao());
+            tab.ColocarPeca(new Torre(tab, Cor.MAGENTA), new PosicaoXadrez('c', 2).toPosicao());
+            tab.ColocarPeca(new Torre(tab, Cor.MAGENTA), new PosicaoXadrez('d', 2).toPosicao());
+            tab.ColocarPeca(new Torre(tab, Cor.MAGENTA), new PosicaoXadrez('e', 2).toPosicao());
+            tab.ColocarPeca(new Torre(tab, Cor.MAGENTA), new PosicaoXadrez('e', 1).toPosicao());
+            tab.ColocarPeca(new Rei(tab, Cor.MAGENTA), new PosicaoXadrez('d', 1).toPosicao());
 
             tab.ColocarPeca(new Torre(tab, Cor.AMARELO), new PosicaoXadrez('c', 7).toPosicao());
             tab.ColocarPeca(new Torre(tab, Cor.AMARELO), new PosicaoXadrez('c', 8).toPosicao());
@@ -131,8 +201,8 @@ if(!tab.peca(origem).podeMoverPara(destino))
             tab.ColocarPeca(new Rei(tab, Cor.AMARELO), new PosicaoXadrez('d', 8).toPosicao());
         }
 
-       
-        
+
+
 
     }
 }
